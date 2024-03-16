97a058e8a8ef7915e0a8f87c0bffa48a869c0103
Konrad Folwarski <konfolw@student.agh.edu.pl>
2024-03-16 10:18:34 +0000

    Update files

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // A map of playerName to an array of playerPER values
-
+var playerMap = new Map()
 
 // Variables to keep track of constants 
-
+const maxPlayersOnCourt = 5;
+const numQuarters = 4;
 
 
 // Variables to track state throughout the game
-
-
+var currentQuarter =0;
+var playersOnCourt = 0;
+var quarterInPlay = false;
 
 
 // Variables to track the PER throughout the game
-
-
+var quarterPER = 0;
+var quarterAvePER = 0;
+var totalAvePER = 0;
 
 
 // Function to read in all of the player stats
