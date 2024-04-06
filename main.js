3074d194def6055ea4aa437df08711edc3220403
Konrad Folwarski <konfolw@student.agh.edu.pl>
2024-04-06 10:45:58 +0000

    adding funcionality of application

--- main.js
+++ main.js
@@ -21,69 +21,70 @@ var totalAvePER = 0;
 // Function to read in all of the player stats
 function processPlayers(allPlayerStats) {
     // Split the data by newline into an array.
-
+    var allPlayerStatLines = allPlayerStats.split(/\r\n|\n/);
 
     // Remove the header line (first line)
-
+    allPlayerStatLines.shift();
 
     // Loop through the rows and create a map entry of player name to a list of player PER
-
+    for (var statLine of allPlayerStatLines) {
         // Get all individual stat values
-
+        var stats = statLine.split(',');
 
         // If it's just an empty line, skip it
-
+        if (!stats || stats.length <= 1) continue; // empty line
 
         // The second column has the player name
-
+        var playerName = stats[1];
 
         // Check if player exists in map
-
+        if (!playerMap.has(playerName)) {
             // First time we see the player; Add them in!
-
-
+            playerMap.set(playerName, []);
+        }
 
         // Get per value for player
-
+        var per = parseFloat(stats[9]);
 
         // Add per value to player's array (the next quarter)
-
+        playerMap.get(playerName).push(per);
+    }
 
     // Add the players to the bench.
-
+    displayPlayerBench();
 }
 
 // Function to add the players to the bench to start the game
 function displayPlayerBench() {
     // Get the bench div in which the players will be shown.
-
+    var bench = document.getElementById('playersOnBench');
 
     // For each player, create a button 
-
+    for (let playerName of playerMap.keys()) {
         // Create a button for each player
-
+        var newPlayer = document.createElement('button');
 
         // Set the ID to the name of the player so we can get it later
-
+        newPlayer.id = playerName;
 
         // Identify the style class, which will set the color scheme
-
+        newPlayer.className = 'playerButton';
 
         // When the button is clicked, call the movePlayer function
-
+        newPlayer.onclick = movePlayer;
 
         // Add the players image to the button
-
+        var playerImage = document.createElement('img');
 
         // Set the source (or location) of the image
-
+        playerImage.src = 'images/'+playerName+'.png';
 
         // Add the image to the button
-
+        newPlayer.appendChild(playerImage);
 
         // Add the button to the bench
-
-
+        bench.appendChild(newPlayer);
+    }
 
     // Display cards for all players
 
